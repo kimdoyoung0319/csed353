28f8edde103574dae06525a29d6611de03332710
Doyoung Kim <kimdoyoung0319@gmail.com>
2025-03-06 13:01:23 +0000

    Implemented a bad version of the reassembler.

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -8,19 +8,64 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _buf(capacity), _output(capacity), _capacity(capacity), _unassembled(0), _eof(UINT64_MAX) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    std::string to_push;
+    size_t buf_index, data_index;
+    uint64_t stream_index;
+    byte_t ch;
+
+    if (eof) {
+        _eof = index + data.size();
+    }
+
+    if (index > _output.bytes_written()) {
+        buf_index = index - _output.bytes_written();
+        data_index = 0;
+    } else {
+        buf_index = 0;
+        data_index = _output.bytes_written() - index;
+    }
+
+    while (_output.buffer_size() + buf_index < _capacity && data_index < data.size()) {
+        if (!_buf[buf_index])
+            _unassembled++;
+
+        _buf[buf_index] = data[data_index];
+
+        buf_index++;
+        data_index++;
+    }
+
+    buf_index = 0;
+    stream_index = _output.bytes_written();
+
+    while (_buf[buf_index] && buf_index < _capacity && stream_index < _eof) {
+        ch = _buf[buf_index].value();
+        to_push.push_back(ch);
+
+        _unassembled--;
+        buf_index++;
+        stream_index++;
+    }
+
+    if (!to_push.empty()) {
+        _output.write(to_push);
+    }
+
+    _buf.erase(_buf.begin(), _buf.begin() + to_push.size());
+    _buf.resize(_capacity);
+
+    if (stream_index >= _eof) {
+        _output.end_input();
+    }
 }
-
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
-
-bool StreamReassembler::empty() const { return {}; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -1,19 +1,23 @@
 #ifndef SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 #define SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
 
+#include "byte.hh"
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <optional>
 #include <string>
+#include <vector>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
-    // Your code here -- add private members as necessary.
-
+    std::vector<std::optional<byte_t>> _buf;
     ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    size_t _capacity;    //!< The capacity of the buffer
+    size_t _unassembled;
+    uint64_t _eof;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -41,11 +45,11 @@ class StreamReassembler {
     //!
     //! \note If the byte at a particular index has been pushed more than once, it
     //! should only be counted once for the purpose of this function.
-    size_t unassembled_bytes() const;
+    size_t unassembled_bytes() const { return _unassembled; };
 
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
-    bool empty() const;
+    bool empty() const { return _unassembled == 0; };
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
