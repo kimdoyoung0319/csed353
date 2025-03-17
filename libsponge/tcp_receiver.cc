a95b19fafe341692d04fdf0d4e8b550a889482bb
Doyoung Kim <kimdoyoung0319@gmail.com>
2025-03-17 07:18:24 +0000

    Initial implementation of TCPReceiver.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,56 @@
 #include "tcp_receiver.hh"
 
-// Dummy implementation of a TCP receiver
+// Implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
+//! \todo Refactor this.
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    bool syn = seg.header().syn;
+    bool fin = seg.header().fin;
+    WrappingInt32 seqno = seg.header().seqno;
+    string payload = seg.payload().copy();
+
+    if (_state == ERROR || _state == FIN_RECV) {
+        throw runtime_error("the receiver is in invalid state");
+    }
+
+    if (syn) {
+        _state = SYN_RECV;
+        _isn = seqno;
+    }
+
+    uint64_t abs_seqno = unwrap(seqno, _isn, _checkpoint) + syn;
+    _reassembler.push_substring(payload, abs_seqno - 1, fin);
+    _checkpoint = abs_seqno;
+
+    if (stream_out().input_ended()) {
+        _state = FIN_RECV;
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    WrappingInt32 ackno = wrap(stream_out().bytes_written() + 1, _isn);
 
-size_t TCPReceiver::window_size() const { return {}; }
+    switch (_state) {
+        case SYN_RECV:
+            return optional(ackno);
+            break;
+        case FIN_RECV:
+            return optional(ackno + 1);
+            break;
+        default:
+            return nullopt;
+            break;
+    }
+}
+
+size_t TCPReceiver::window_size() const {
+    size_t stream_start = stream_out().bytes_read();
+    size_t stream_end = stream_out().bytes_written();
+
+    return stream_start + _capacity - stream_end;
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -14,18 +14,36 @@
 //! the acknowledgment number and window size to advertise back to the
 //! remote TCPSender.
 class TCPReceiver {
+    //! Possible states of the receiver.
+    //! \todo Is this enum really needed?
+    enum State {
+        LISTEN,
+        SYN_RECV,
+        FIN_RECV,
+        ERROR,
+    };
+
+    //! Internal state.
+    State _state = LISTEN;
+
     //! Our data structure for re-assembling bytes.
     StreamReassembler _reassembler;
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    //! The inital sequence number.
+    WrappingInt32 _isn;
+
+    //! The sequence number of the last segment that has been received.
+    uint64_t _checkpoint;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _isn(0), _checkpoint(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
