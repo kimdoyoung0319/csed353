c9b646091be3992e0509d41b7584acc4e93773b9
Doyoung Kim <kimdoyoung0319@gmail.com>
2025-04-01 07:03:49 +0000

    Fixed merge errors.

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -18,7 +18,6 @@ using namespace std;
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
-    , _retransmission_timeout{retx_timeout}
     , _stream(capacity) {}
 
 uint64_t TCPSender::bytes_in_flight() const { return _next_seqno - _ackno; }
